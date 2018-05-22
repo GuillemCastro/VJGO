03ebf1e06ead91bd477767add4ffbed183c2b285
Guillem Castro <guillem.castro@hotmail.com>
2018-05-22 13:12:20 +0200

    Redstone

---
+++ VJGO/Assets/Scripts/Redstone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Redstone : MonoBehaviour {
+
+    public Material ActiveMaterial;
+    public Material NormalMaterial;
+
+	void Start () {
+	}
+	
+	public void Activate()
+    {
+        GetComponent<Renderer>().material = ActiveMaterial;
+    }
+
+    public void DeActivate()
+    {
+        GetComponent<Renderer>().material = NormalMaterial;
+    }
+}
