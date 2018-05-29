c606e7bedae868820da144981f331f08ef8aeee5
Guillem Castro <guillem.castro@hotmail.com>
2018-05-29 21:06:17 +0200

    Fixed end screen

--- VJGO/Assets/Scripts/EndScreen.cs
+++ VJGO/Assets/Scripts/EndScreen.cs
@@ -7,6 +7,7 @@ public class EndScreen : MonoBehaviour {
 
     Button next;
     Button restart;
+    Text labelStats;
     Text label;
 
     StatsManager statsManager;
@@ -16,21 +17,22 @@ public class EndScreen : MonoBehaviour {
         statsManager = Object.FindObjectOfType<StatsManager>().GetComponent<StatsManager>();
         next = transform.Find("RestartButton/NextButton").GetComponent<Button>();
         restart = transform.Find("RestartButton").GetComponent<Button>();
-        label = transform.Find("MissionCard/Border/Background/LabelStats").GetComponent<Text>();
+        labelStats = transform.Find("MissionCard/Border/Background/LabelStats").GetComponent<Text>();
+        label = transform.Find("MissionCard/Border/Background/Label").GetComponent<Text>();
     }
 
     public void Win()
     {
         this.enabled = true;
         string text = string.Format("{0} enemies killed\n{1} diamonds collected", statsManager.EnemiesKilled, statsManager.DiamondsCollected);
-        label.text = text;
+        labelStats.text = text;
     }
 
     public void Lose()
     {
         this.enabled = true;
         next.interactable = false;
-        label.enabled = false;
+        labelStats.enabled = false;
         label.text = "YOU\nLOST\n:c";
     }
 
