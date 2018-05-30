4617f5210ef04f325bdf87aefea27a41430ea2b3
Guillem Castro <guillem.castro@hotmail.com>
2018-05-30 20:02:21 +0200

    God Mode v2 (faster Player & Enemies) + minor UI changes

--- VJGO/Assets/Scripts/GodModeManager.cs
+++ VJGO/Assets/Scripts/GodModeManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GodModeManager : MonoBehaviour {
 
@@ -10,12 +11,42 @@ public class GodModeManager : MonoBehaviour {
 
     public bool IsGodModeActive { get { return m_godMode; } }
 
+    PlayerMover playerMover;
+    List<EnemyMover> enemyMovers;
+
     private void Awake()
     {
+        Debug.Log("GM Awake");
         if (!created)
         {
             created = true;
             DontDestroyOnLoad(this.gameObject);
+        }
+        SceneManager.sceneLoaded += this.LevelStart;
+    }
+
+    private void Start()
+    {
+        Debug.Log("GM START");
+    }
+
+    private void LevelStart(Scene scene, LoadSceneMode sceneMode)
+    {
+        playerMover = Object.FindObjectOfType<PlayerMover>();
+        enemyMovers = new List<EnemyMover>(Object.FindObjectsOfType<EnemyMover>());
+        if (IsGodModeActive)
+        {
+            if (playerMover != null)
+            {
+                playerMover.moveSpeed = 5;
+            }
+            if (enemyMovers != null)
+            {
+                foreach (EnemyMover em in enemyMovers)
+                {
+                    em.moveSpeed = em.moveSpeed * 2.5f;
+                }
+            }
         }
     }
 
